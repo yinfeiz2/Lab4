81ba6eed6549f2bcfe377b65fe39a81e80d4cb26
jerry <jerry@wirelessprvnat-172-16-224-57.near.illinois.edu>
2017-09-26 20:22:54 -0500

    corrected code

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,34 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * a function to find the same part of two strings.
+ * <p>
+ */
+public class ReclamationProject {
+/**
+ * @param first the first string
+ * @param second the second string
+ * @return return the same part
+ */
+    static String doit(final String first, final String second) {
+        String a = first;
+        String b = second;
+        if (first.length() > second.length()) {
+            String c = first;
+            a = second;
+            b = c;
+            }
+        String r = "";
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                    }
+                        }
+                }
+        return r;
+        }
 }
